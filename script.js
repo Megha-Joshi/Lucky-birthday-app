01337b37d9eab2a14b4005d1eade06009dfbbd69
Megha-Joshi <megha24joshi@gmail.com>
2021-12-04 04:07:27 -0800

    refractor

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ function checkIfBirthdateIsLucky() {
 	const bdate = DOB.value;
 	const ln = Number(LN.value);
 	const sum = checkSum(bdate);
-
 	if (sum && ln) {
 
 
-		if (sum % ln === 0) {
+		if ((sum % ln === 0) && (ln > 0)) {
 			opMessage.innerText = "Congrats 🤩 You have a lucky birthdate";
+		} else if (ln < 0) {
+			opMessage.innerText = "Enter the lucky number greater than o";
 		} else {
 			opMessage.innerText = "Sorry 😐 Not a lucky birthdate";
 		}
